267373c3931dc0b941e316d8059e8f1a353da769
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-10 15:51:08 +0530

    Adding some chages in the code.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,24 +9,22 @@ function Navbar() {
   const [toggle, setToggle] = useState(false);
   return (
     <>
-      <div className='slide-container'>
-        <div className='main-div'>
-          <nav className='app__navbar'>
-            <p className='brand-name'>Aloha</p>
-            <ul className='app__navbar-links'>
-              {['Home', 'Surfing', 'Hula', 'Vulcano'].map((item) => (
-                <li className='app__flex p-text' key={`link-${item}`}>
-                  <div />
-                  <a href={`#${item}`}>{item}</a>
-                </li>
-              ))}
-            </ul>
+      <div className='main-div'>
+        <nav className='app__navbar'>
+          <p className='brand-name'>Aloha</p>
+          <ul className='app__navbar-links'>
+            {['Home', 'Surfing', 'Hula', 'Vulcano'].map((item) => (
+              <li className='app__flex p-text' key={`link-${item}`}>
+                <div />
+                <a href={`#${item}`}>{item}</a>
+              </li>
+            ))}
+          </ul>
 
-            <div className='app__navbar-menu'>
-              <button className='btn'>Book a trip</button>
-            </div>
-          </nav>
-        </div>
+          <div className='app__navbar-menu'>
+            <button className='btn'>Book a trip</button>
+          </div>
+        </nav>
       </div>
     </>
   );
