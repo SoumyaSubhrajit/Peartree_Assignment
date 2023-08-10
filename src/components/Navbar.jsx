9ca54a492adc9f1985094bacdc2bf9f6b33cc612
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-10 13:23:54 +0530

    Adding some more code into the database.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,46 +3,32 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import './Navbar.css';
+import image1 from '../images/Image.png';
 
 function Navbar() {
   const [toggle, setToggle] = useState(false);
   return (
-    <div className='main-div'>
-      <nav className='app__navbar'>
-        <ul className='app__navbar-links'>
-          {['Home', 'Surfing', 'Hula', 'Vulcano'].map((item) => (
-            <li className='app__flex p-text' key={`link-${item}`}>
-              <div />
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className='slide-container'>
+        <div className='main-div'>
+          <nav className='app__navbar'>
+            <p className='brand-name'>Aloha</p>
+            <ul className='app__navbar-links'>
+              {['Home', 'Surfing', 'Hula', 'Vulcano'].map((item) => (
+                <li className='app__flex p-text' key={`link-${item}`}>
+                  <div />
+                  <a href={`#${item}`}>{item}</a>
+                </li>
+              ))}
+            </ul>
 
-        <div className='app__navbar-menu'>
-          <button className='btn'>Book a trip</button>
+            <div className='app__navbar-menu'>
+              <button className='btn'>Book a trip</button>
+            </div>
+          </nav>
         </div>
-
-        {/* <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)} />
-          {toggle && (
-            <motion.div
-              whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.85, ease: 'easeOut' }}
-            >
-              <HiX onClick={() => setToggle(false)} />
-              <ul className='app__navbar-links'>
-                {['Home', 'Surfing', 'Hula', 'Vulcano'].map((item) => (
-                  <li className='app__flex p-text' key={`link-${item}`}>
-                    <div />
-                    <a href={`#${item}`}>{item}</a>
-                  </li>
-                ))}
-              </ul>
-            </motion.div>
-          )}
-        </div> */}
-      </nav>
-    </div>
+      </div>
+    </>
   );
 }
 
