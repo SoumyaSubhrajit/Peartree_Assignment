28e6ca91c6341582f70ec90edc9da9f8d3ccf235
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-11 01:15:58 +0530

    Adding some code here.

--- src/components/Highlights/Card.jsx
+++ src/components/Highlights/Card.jsx
@@ -12,23 +12,86 @@ function Card() {
           {' '}
           <img src={Tile1} alt='Tile1' />
         </div>
-        <h3>Surfing</h3>
-        <p>Best Hawaiian islands for surfing.</p>
 
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='app__arrow-btn'
-          viewBox='0 0 512 512'
-        >
-          <path
-            fill='none'
-            stroke='currentColor'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-            stroke-width='48'
-            d='M268 112l144 144-144 144M392 256H100'
-          />
-        </svg>
+        <div className='app__card-content'>
+          <h3>Surfing</h3>
+          <p>Best Hawaiian islands for surfing.</p>
+        </div>
+
+        <div className='app__arrow'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='app__arrow-btn'
+            viewBox='0 0 512 512'
+          >
+            <path
+              fill='none'
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='48'
+              d='M268 112l144 144-144 144M392 256H100'
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div className='app__card'>
+        <div className='image'>
+          {' '}
+          <img src={Tile2} alt='Tile1' />
+        </div>
+
+        <div className='app__card-content'>
+          <h3>Surfing</h3>
+          <p>Best Hawaiian islands for surfing.</p>
+        </div>
+
+        <div className='app__arrow'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='app__arrow-btn'
+            viewBox='0 0 512 512'
+          >
+            <path
+              fill='none'
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='48'
+              d='M268 112l144 144-144 144M392 256H100'
+            />
+          </svg>
+        </div>
+      </div>
+
+      <div className='app__card'>
+        <div className='image'>
+          {' '}
+          <img src={Tile3} alt='Tile1' />
+        </div>
+
+        <div className='app__card-content'>
+          <h3>Surfing</h3>
+          <p>Best Hawaiian islands for surfing.</p>
+        </div>
+
+        <div className='app__arrow'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='app__arrow-btn'
+            viewBox='0 0 512 512'
+          >
+            <path
+              fill='none'
+              stroke='currentColor'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='48'
+              d='M268 112l144 144-144 144M392 256H100'
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
