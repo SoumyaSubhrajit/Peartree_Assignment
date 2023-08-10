9bad2f94240c30111bc7ab5aec6e90bf96874cf5
Soumya Subhrajit Bag <soumyasubhrajit@gmail.com>
2023-08-10 23:10:50 +0530

    Adding some more style to the database.

--- src/components/Highlights/Card.jsx
+++ src/components/Highlights/Card.jsx
@@ -6,31 +6,29 @@ import './Card.css';
 
 function Card() {
   return (
-    <div>
-      <div className='app__main-div'>
-        <div className='app__cards'>
-          <div className='app__card'>
-            <img src={Tile1} alt='Tile1' />
-            <h3>Surfing</h3>
-            <p>Best Hawaiian islands for surfing.</p>
-          </div>
-          <div className='app__arrow-btn'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='app__arrow-btn'
-              viewBox='0 0 512 512'
-            >
-              <path
-                fill='none'
-                stroke='currentColor'
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                stroke-width='48'
-                d='M268 112l144 144-144 144M392 256H100'
-              />
-            </svg>
-          </div>
+    <div className='app__cards'>
+      <div className='app__card'>
+        <div className='image'>
+          {' '}
+          <img src={Tile1} alt='Tile1' />
         </div>
+        <h3>Surfing</h3>
+        <p>Best Hawaiian islands for surfing.</p>
+
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='app__arrow-btn'
+          viewBox='0 0 512 512'
+        >
+          <path
+            fill='none'
+            stroke='currentColor'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-width='48'
+            d='M268 112l144 144-144 144M392 256H100'
+          />
+        </svg>
       </div>
     </div>
   );
